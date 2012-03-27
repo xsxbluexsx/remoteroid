bf1717359778a913c2ff2c22b0e21166c8e5c07e
jyte82@gmail.com <jyte82@gmail.com@9f4742bf-7701-a943-8b4a-450bfbe88ad2>
2012-03-27 06:57:28 +0000

    Added IRemoteroid for Notification passing

--- trunk/android/src/org/secmem/remoteroid/service/RemoteroidService.java
+++ trunk/android/src/org/secmem/remoteroid/service/RemoteroidService.java
@@ -1,15 +1,26 @@
 package org.secmem.remoteroid.service;
 
+import org.secmem.remoteroid.IRemoteroid;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 
 public class RemoteroidService extends Service {
+	
+	private IBinder mBinder = new IRemoteroid.Stub() {
+		
+		@Override
+		public void onNotificationCatched(String notificationText, long when)
+				throws RemoteException {
+			// TODO Notification hooked and notification data has been delivered. Now this data should be sent to PC.
+		}
+	};
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
+		return mBinder;
 	}
 
 }
