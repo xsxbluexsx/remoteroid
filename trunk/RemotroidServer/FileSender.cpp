aede559d31510daf9bf800e561c5df8534f557e9
ohmnia1112@gmail.com <ohmnia1112@gmail.com@9f4742bf-7701-a943-8b4a-450bfbe88ad2>
2012-05-03 15:52:38 +0000

    file Receiver and FileSender Drag and Drop File Trans complete

--- trunk/RemotroidServer/FileSender.cpp
+++ trunk/RemotroidServer/FileSender.cpp
@@ -2,9 +2,11 @@
 #include "FileSender.h"
 
 
-CFileSender::CFileSender(CMyClient *pClient)
-	: m_pClient(pClient)
+CFileSender::CFileSender()
+	: totalFileSize(0), m_pClient(NULL),
+	sendedFileSize(0),isSending(FALSE)	
 {
+	memset(buffer, 0, sizeof(buffer));
 }
 
 
@@ -12,6 +14,10 @@ CFileSender::~CFileSender(void)
 {
 }
 
+void CFileSender::SetClient(CMyClient *pClient)
+{
+	m_pClient = pClient;
+}
 
 int CFileSender::SendPacket(int iOPCode, const char * data, int iDataLen)
 {
@@ -19,10 +25,75 @@ int CFileSender::SendPacket(int iOPCode, const char * data, int iDataLen)
 }
 
 
-void CFileSender::SendFileInfo(HANDLE hSendFile)
-{
-	m_hSendFile = hSendFile;
-	DWORD dwFileSizeHigh;
-	//DWORD dwFileSizeLow = GetFileSize(hFile, &dwFileSizeHigh);
+int CFileSender::SendFileInfo(CFile *pFile)
+{	
+	totalFileSize = pFile->GetLength();
+	CString fileName = pFile->GetFileName();
+
+	//유니코드 형식의 파일 이름을 UTF-8로 변환
+	TCHAR uniFileName[FILENAMESIZE];
+	char utfFileName[FILENAMESIZE];	
+	memset(uniFileName, 0, sizeof(uniFileName));
+	memset(utfFileName, 0, sizeof(utfFileName));
+	_tcscpy(uniFileName, fileName);
+	CUtil::UniToUtf(uniFileName, utfFileName);
+
+	//프로토콜에 맞춰서 상위 100바이트는 파일 이름 이후 100바이트는 파일 크기
 	
+	memset(buffer, 0, sizeof(buffer));
+	memcpy(buffer, utfFileName, FILENAMESIZE);
+	sprintf(buffer+FILENAMESIZE, "%100llu", totalFileSize);
+
+	sendedFileSize = 0;
+	return SendPacket(OP_SENDFILEINFO, buffer, FILENAMESIZE+FILESIZESIZE);
 }
+
+int CFileSender::SendFileData(CFile * pFile)
+{
+	int result = 0;	
+	while(totalFileSize > sendedFileSize)
+	{
+		int iCurrentSendSize = (totalFileSize-sendedFileSize) > MAXDATASIZE ?
+			MAXDATASIZE : totalFileSize-sendedFileSize;
+		pFile->Read(buffer, iCurrentSendSize);
+		result = SendPacket(OP_SENDFILEDATA, buffer, iCurrentSendSize);
+		sendedFileSize += iCurrentSendSize;
+	}
+	pFile->Close();
+	return result;
+}
+
+
+
+void CFileSender::AddSendFile(CFile * pFile)
+{
+	sendFileList.AddTail((void*)pFile);	
+}
+
+
+BOOL CFileSender::StartSendFile(void)
+{
+	if(m_pClient == NULL || isSending == TRUE)
+		return FALSE;
+
+	isSending = TRUE;
+	AfxBeginThread(SendFileThread, this);
+	return TRUE;
+}
+
+
+UINT CFileSender::SendFileThread(LPVOID pParam)
+{	
+	CFileSender *pDlg = (CFileSender *)pParam;	
+
+ 	POSITION pos = pDlg->sendFileList.GetHeadPosition();
+	while(pos)
+	{
+		CFile *pFile = (CFile *)pDlg->sendFileList.GetNext(pos);
+		pDlg->SendFileInfo(pFile);
+		pDlg->SendFileData(pFile);
+	}
+	pDlg->isSending = FALSE;
+	return 0;
+}
+
