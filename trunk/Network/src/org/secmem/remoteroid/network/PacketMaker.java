ccff65c68e79dd78087c51d2568f89ac7a820584
ohmnia1112 <ohmnia1112@9f4742bf-7701-a943-8b4a-450bfbe88ad2>
2012-05-03 16:00:21 +0000

    File receiver and File Sender complete

--- trunk/Network/src/org/secmem/remoteroid/network/PacketMaker.java
+++ trunk/Network/src/org/secmem/remoteroid/network/PacketMaker.java
@@ -5,12 +5,13 @@ import java.io.*;
 import android.util.*;
 
 public class PacketMaker implements packetmakeable{	
-	private byte [] packet = new byte[CONS.MAXPACKETSIZE];
-	OutputStream out = null;
+	private byte[] 			packet 		= new byte[CONS.MAXPACKETSIZE];
+	OutputStream 			out 		= null;	
+	private StringBuilder 	strHeader 	= new StringBuilder(CONS.HEADERSIZE);
 	
 	public PacketMaker(OutputStream out) {
 		// TODO Auto-generated constructor stub
-		this.out =out;  
+		this.out =out;		
 	}
 	
 	/**
@@ -18,22 +19,23 @@ public class PacketMaker implements packetmakeable{
 	 */
 	public boolean SendPacket(int iOPCode, byte [] data, int length){	
 			int iTotalSize = length + CONS.HEADERSIZE;
-			StringBuilder strHeader = new StringBuilder(6);
-			strHeader.append(String.format("%2d", iOPCode));
-			strHeader.append(String.format("%4d", iTotalSize));
-			//프로토콜에 따라 헤더 조립 (2자리수 opcode + 4자리수 totalsize)			
 			//헤더 싸이즈와 데이터 싸이즈를 더해 totalsize 계산
+			
+			strHeader.replace(0, CONS.OPCODESIZE, String.format("%2d", iOPCode));
+			strHeader.replace(CONS.OPCODESIZE, CONS.HEADERSIZE, String.format("%4d", iTotalSize));
+			//프로토콜에 따라 헤더 조립 (2자리수 opcode + 4자리수 totalsize)			
+
 			byte [] header = strHeader.toString().getBytes();
 			System.arraycopy(header, 0, packet, 0, header.length);
 			System.arraycopy(data, 0, packet, header.length, data.length);
+
 			try{
-				out.write(packet, 0, iTotalSize);
-				Log.i("q", iTotalSize+"");
+				out.write(packet, 0, iTotalSize);				
 			}catch(IOException e){
 				//서버와 접속이 끈겼을 경우
+				Log.i("exception", e.getMessage());
 				return false;
 			}
 			return true;
-	}
-	
+	}	
 }
