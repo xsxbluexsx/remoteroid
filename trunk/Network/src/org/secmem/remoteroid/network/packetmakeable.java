9d5e7766551a5534c9a503768191c8c37aeab7f4
jyte82@gmail.com <jyte82@gmail.com@9f4742bf-7701-a943-8b4a-450bfbe88ad2>
2012-05-24 14:54:05 +0000

    This content was merged with main project(android) long long ago...

--- trunk/Network/src/org/secmem/remoteroid/network/packetmakeable.java
+++
@@ -1,8 +0,0 @@
-package org.secmem.remoteroid.network;
-
-import java.io.*;
-
-interface packetmakeable {
-	//hyomin
-	public void SendPacket(int iOPCode, byte [] data, int length) throws IOException;
-}
