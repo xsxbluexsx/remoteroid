d4565baf347277f60035eb758f49b352caa54dae
godgjdgjd@gmail.com <godgjdgjd@gmail.com@9f4742bf-7701-a943-8b4a-450bfbe88ad2>
2012-09-18 19:15:08 +0000

    add ClipBoard

--- android/src/org/secmem/remoteroid/service/RemoteroidService.java
+++ android/src/org/secmem/remoteroid/service/RemoteroidService.java
@@ -50,6 +50,7 @@ import android.app.PendingIntent;
 import android.app.PendingIntent.CanceledException;
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
